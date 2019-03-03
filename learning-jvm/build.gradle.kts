03f83ec68fe9bf1bdeb684f83c66e825ee9861fa
Vincent <luxx@ytx.com>
2019-03-03 23:04:19 +0800

    JVM: some dependencies.

---
+++ learning-jvm/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("org.springframework.boot") version "2.1.3.RELEASE"
+}
+
+apply(plugin="io.spring.dependency-management")
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    compileOnly("org.projectlombok:lombok")
+    testImplementation("junit:junit:4.12")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
